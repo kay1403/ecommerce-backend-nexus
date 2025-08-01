449873684c69848b60baab803b4e0c59248ca526
Ange Koumba <angekoumba754@gmail.com>
2025-08-02 01:07:15 +0200

    updating

--- products/views.py
+++ products/views.py
@@ -4,26 +4,23 @@ from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# ViewSet pour les catégories
 class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all().order_by('id') 
+    queryset = Category.objects.all().order_by('id')  # <-- Ajouté pour éviter le crash
     serializer_class = CategorySerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['name', 'slug']  # Permet la recherche par nom ou slug
+    search_fields = ['name', 'slug']
 
-# ViewSet pour les produits
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all().order_by('id')
+    queryset = Product.objects.all().order_by('id')  # <-- Ajouté ici aussi
     serializer_class = ProductSerializer
 
-    # Ajout des fonctionnalités de filtrage, recherche et tri
     filter_backends = [
-        DjangoFilterBackend,     # Pour les filtres exacts (ex: ?category=1)
-        filters.OrderingFilter,  # Pour le tri (ex: ?ordering=price)
-        filters.SearchFilter     # Pour la recherche (ex: ?search=chaussure)
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter
     ]
 
-    filterset_fields = ['category']  # Filtres exacts autorisés
-    ordering_fields = ['price', 'name']  # Champs autorisés pour le tri
-    search_fields = ['name', 'description']  # Champs recherchables
+    filterset_fields = ['category']
+    ordering_fields = ['price', 'name']
+    search_fields = ['name', 'description']
     permission_classes = [IsAuthenticatedOrReadOnly]
