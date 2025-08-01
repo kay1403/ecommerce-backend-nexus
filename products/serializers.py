1cc021a23f3b46f84573cb101f2eeda94ee17258
Ange Koumba <angekoumba754@gmail.com>
2025-08-02 00:45:05 +0200

    updating

--- products/serializers.py
+++ products/serializers.py
@@ -27,3 +27,11 @@ class ProductSerializer(serializers.ModelSerializer):
             'category',
             'category_id',
         ]
+    def create(self, validated_data):
+        return Product.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance    
