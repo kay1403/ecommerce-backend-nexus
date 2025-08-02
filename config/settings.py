55fb82b7b00db0a4d221a598074833a8db1f811f
Ange Koumba <angekoumba754@gmail.com>
2025-08-02 23:38:09 +0200

    updating

--- config/settings.py
+++ config/settings.py
@@ -102,6 +102,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
